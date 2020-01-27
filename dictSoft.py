8c3ab5152ad0e041ac4175f0b1cf16e5d6780b5d
damnpav <32949863+damnpav@users.noreply.github.com>
2020-01-27 20:51:12 +0300

    set OOP

--- dictSoft.py
+++ dictSoft.py
@@ -10,84 +10,75 @@ import pandas as pd
 import time
 from random import randint
 from random import shuffle
-from googletrans import Translator
+import re
+
+dictPath = "dict.csv"  # route to words
 
 class Game():
-    def __init__(self, dictPath, countOfRounds):
+    def __init__(self, dictPath):
         self.dictPath = dictPath
-        self.countOfRounds = countOfRounds
         self.badAnswers = ['Lol, no', 'Try better', 'U made me cry, loser', 'Nope']
         self.yesAnswers = ['Good!', 'Hell, yeah!', '^=^ you are the best!', 'Yep']
+        self.usedIndexes = []  # array with used indexes of words from dict
+        self.score = 0
 
     def updateDict(self):
-        dictDf = pd.read_csv(dictPath, sep=";")
+        dictDf = pd.read_csv(self.dictPath, sep=";")
         dictDf = dictDf.fillna("")
         self.words = dictDf['word'].tolist()
         self.transcript = dictDf['transcript'].tolist()
         self.translate = dictDf['translate'].tolist()
 
     def play(self):
-
-
-
-
-def updateDict(rout):
-    dictDf = pd.read_csv(rout, sep = ";")
-    dictDf = dictDf.fillna("")
-    words = dictDf['word'].tolist()
-    transcript = dictDf['transcript'].tolist()
-    translate = dictDf['translate'].tolist()
-    return([words, transcript, translate])
-
-def training(words, transcript, translate):
-    print("How many rounds do you want? (type integer digit)")
-    count = int(input()) # count of rounds
-    #rate = 0
-    pastmyIndexes = [] # что это такое блят
-    badAnswers = ['Lol, no', 'Try better', 'U made me cry, loser', 'Nope']
-    yesAnswers = ['Good!', 'Hell, yeah!', '^=^ you are the best!', 'Yep']
-    score = 0
-    for i in range(count):
-        print("Okay dude, be patient...\n")
-        time.sleep(1)                                                                                                                                                                                                                                                              
         flag = 0
         while flag == 0:
-            myIndex = randint(0, len(words))
-            firstWrong = randint(0, len(words))
-            secondWrong = randint(0, len(words)) 
-            thirdWrong = randint(0, len(words))
-            if myIndex in pastmyIndexes or myIndex in [firstWrong, secondWrong, thirdWrong]:
+            myIndex = randint(0, len(self.words))
+            firstWrong = randint(0, len(self.words))
+            secondWrong = randint(0, len(self.words))
+            thirdWrong = randint(0, len(self.words))
+            if myIndex in self.usedIndexes or myIndex in [firstWrong, secondWrong, thirdWrong]:
                 pass
             else:
-                pastmyIndexes.append(myIndex)
+                self.usedIndexes.append(myIndex)
                 flag = 1
-                
-        print("I have this word: \n", words[myIndex], " ", transcript[myIndex], "\n")
-        print("Choose the right translation: (type integer digit from 1 to 4)")
+        print("I have this word: \n", self.words[myIndex], " ", self.transcript[myIndex])
+        print("Choose the right translation: (type digit from 1 to 4)")
         time.sleep(2)
-        
-        order = [str(translate[firstWrong]), \
-                 str(translate[secondWrong]), \
-                 str(translate[thirdWrong]), \
-                 str(translate[myIndex])]
-        shuffle(order) # random mixing of translations
-        rightIndex = order.index(str(translate[myIndex])) + 1 # detect number of corrrect answer
-        
+        order = [str(self.translate[firstWrong]), \
+             str(self.translate[secondWrong]), \
+             str(self.translate[thirdWrong]), \
+             str(self.translate[myIndex])]
+        shuffle(order)  # random mixing of translations
+        rightIndex = order.index(str(self.translate[myIndex])) + 1  # detect number of corrrect answer
+
         print("1 ", order[0])
-        print("2 ", order[1])        
+        print("2 ", order[1])
         print("3 ", order[2])
         print("4 ", order[3])
-        
-        userAnswer = int(input())
-        time.sleep(1)  
-        if userAnswer == rightIndex:
-            print(yesAnswers[randint(0, len(yesAnswers)-1)], "\n")
-            score += 1
-        else:
-            print(badAnswers[randint(0, len(badAnswers)-1)], "\n The right answer is: ", (rightIndex))
-    print("Your score is: ", score, "/", count)
-            
-        
 
-check = updateDict(path)
-training(check[0], check[1], check[2])
+        userAnswer = int(input())
+        time.sleep(1)
+        if userAnswer == rightIndex:
+            print(self.yesAnswers[randint(0, len(self.yesAnswers) - 1)], "\n")
+            self.score += 1
+        else:
+            print(self.badAnswers[randint(0, len(self.badAnswers) - 1)], "\n The right answer is: ", (rightIndex))
+
+def main():
+    print("How many rounds do you want to play?")
+    flag = 0
+    while flag == 0:
+        print("Type the digit from 1 to 9. X to exit")
+        userInput = input()
+        if re.match('[1-9]', userInput) != None:
+            flag = 1
+        elif userInput.lower() == 'x':
+            return "Bye!"
+    myGame = Game(dictPath)
+    myGame.updateDict()  # update dictionary with words
+    for i in range(int(userInput)):
+        myGame.play()
+    return "Your score is: " + str(myGame.score)
+
+print(main())
+
