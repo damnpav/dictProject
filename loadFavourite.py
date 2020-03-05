d9a2f479f11b13d474449c4778f674ec9a2fe45d
damnpav <32949863+damnpav@users.noreply.github.com>
2020-03-06 01:12:07 +0300

    loading new dict

---
+++ loadFavourite.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# i need to move data from one csv to another
+class FavouriteDict():
+    def __init__(self, favDictPath, mainDictPath):
+        self.favDictPath = favDictPath
+        self.mainDictPath = mainDictPath
+# TODO разберись с кодировками!!!
+    def loadNewWords(self):
+        favPd = pd.read_csv(self.favDictPath, sep=';', header=None)
+        mainPd = pd.read_csv(self.mainDictPath, sep=';')
+        newDf = pd.DataFrame(list(zip(list(favPd[2]), [''] * favPd.shape[0], list(favPd[3]))),\
+                             columns=['word', 'transcript', 'translate'])
+        mainPd = mainPd.append(newDf, ignore_index=True)
+        mainPd.to_csv(self.mainDictPath, index=False)
+
+
+
+
+
